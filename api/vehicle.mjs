b648dc055e95c8d59d31225baecd7484f6c146af
Abdul-asa <bigtulahi1@gmaiil.com>
2024-05-01 07:30:55 +0100

    Refactor vehicle.mjs to use query parameters instead of request parameters

--- api/vehicle.mjs
+++ api/vehicle.mjs
@@ -1,25 +1,30 @@
 import { api } from "./mockData.mjs";
 export default async function handler(req, res) {
-  const { regNumber } = req.params;
-  const response = await fetch(process.env.API_URL, {
-    method: "POST",
-    headers: {
-      "x-api-key": process.env.API_KEY,
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({ registrationNumber: regNumber }),
+  const query = req.query;
+  const regNumber = query.regNumber;
+  res.status(200).json({
+    message: "Hello from serverless!",
+    regNumber: regNumber,
   });
+  //   const response = await fetch(process.env.API_URL, {
+  //     method: "POST",
+  //     headers: {
+  //       "x-api-key": process.env.API_KEY,
+  //       "Content-Type": "application/json",
+  //     },
+  //     body: JSON.stringify({ registrationNumber: regNumber }),
+  //   });
 
-  const data = await response.json();
-  if (data.errors) {
-    const isInMockData = api.find(
-      (car) => car.registrationNumber === regNumber
-    );
-    if (isInMockData) {
-      res.json(isInMockData);
-      return;
-    }
-  }
-  // console.log(data);
-  res.json(data);
+  //   const data = await response.json();
+  //   if (data.errors) {
+  //     const isInMockData = api.find(
+  //       (car) => car.registrationNumber === regNumber
+  //     );
+  //     if (isInMockData) {
+  //       res.json(isInMockData);
+  //       return;
+  //     }
+  //   }
+  //   // console.log(data);
+  //   res.json(data);
 }
